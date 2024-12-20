4f3e21cf3939a2e3aba885ae68a3431fd1bbaa2f
MikeNakis <git@michael.gr>
2024-12-20 17:03:01 +0100

    make use of MikeNakis.GlobalConfig

--- MikeNakis.Kit/build.bat
+++ MikeNakis.Kit/build.bat
@@ -1,6 +1,6 @@
 call vsclean
-set VERSION=1.0.331
+set VERSION=1.0.33.1
 dotnet restore
 dotnet build --configuration Release --no-restore
-dotnet pack --configuration Release --no-build --version-suffix=10.1
+dotnet pack --configuration Release --no-build
 dotnet nuget push bin/Release/MikeNakis.Kit.%VERSION%.nupkg --source https://api.nuget.org/v3/index.json --api-key %NUGET_API_KEY%
