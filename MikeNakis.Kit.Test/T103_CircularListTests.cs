9ac5c52a7a3329b99416a4a206363ce89a8bf6c7
MikeNakis <git@michael.gr>
2025-03-11 16:03:25 +0100

    introduced circularlist

---
+++ MikeNakis.Kit.Test/T103_CircularListTests.cs
@@ -0,0 +1,294 @@
+namespace MikeNakis.Kit.Test;
+
+using Sys = System;
+using System.Linq;
+using MikeNakis.Kit.Collections;
+using static MikeNakis.Kit.GlobalStatics;
+using VSTesting = Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[VSTesting.TestClass]
+public sealed class T103_CircularListTests
+{
+	[VSTesting.TestMethod]
+	public void SimpleTest()
+	{
+		CircularList<int> list = new();
+		list.Add( 3 );
+		Assert( list[0] == 3 );
+		list.Add( 4 );
+		Assert( list[1] == 4 );
+		Assert( list.IndexOf( 3 ) == 0 );
+		list.Remove( 3 );
+		Assert( list.Count == 1 );
+		Assert( list[0] == 4 );
+	}
+
+	[VSTesting.TestMethod]
+	public void RemoveAtTest()
+	{
+		CircularList<int> list = new( 2 );
+		for( int i = 0; i < 8; i++ )
+			list.Add( i );
+		list.RemoveAt( 5 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 1, 2, 3, 4, 6, 7 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 2, 3, 4, 6, 7 ) ) );
+		list.RemoveAt( 3 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 2, 3, 6, 7 ) ) );
+		list.RemoveAt( 2 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 2, 6, 7 ) ) );
+		list.RemoveAt( 2 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 2, 7 ) ) );
+		list.RemoveAt( 2 );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 2 ) ) );
+		list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 2 ) ) );
+		list.RemoveAt( 0 );
+		int value = 0;
+		Sys.Exception? thrownException = TryCatch( () => value = list[0] );
+		Assert( thrownException is AssertionFailureException );
+	}
+
+	[VSTesting.TestMethod]
+	public void RemoveAtTest2()
+	{
+		CircularList<int> list = new( 2 );
+		for( int i = 0; i < 8; i++ )
+			list.Add( i );
+		Assert( list.SequenceEqual( EnumerableOf( 0, 1, 2, 3, 4, 5, 6, 7 ) ) );
+		for( int i = 0; i < 6; i++ )
+			list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 7 ) ) );
+		list.Add( 8 );
+		list.Add( 9 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 7, 8, 9 ) ) );
+		list.RemoveAt( 2 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 7, 9 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 9 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 6 ) ) );
+		Sys.Exception? thrownException = TryCatch( () => list.RemoveAt( 1 ) );
+		Assert( thrownException is AssertionFailureException );
+		list.RemoveAt( 0 );
+		int value = 0;
+		thrownException = TryCatch( () => value = list[0] );
+		Assert( thrownException is AssertionFailureException );
+	}
+
+	[VSTesting.TestMethod]
+	public void RemoveAtTest3()
+	{
+		CircularList<int> list = new( 2 );
+		for( int i = 0; i < 8; i++ )
+			list.Add( i );
+		for( int i = 0; i < 6; i++ )
+			list.RemoveAt( 0 );
+		list.Add( 8 );
+		list.Add( 9 );
+		list.Add( 10 );
+		list.Add( 11 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 7, 8, 9, 10, 11 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 8, 9, 10, 11 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 6, 9, 10, 11 ) ) );
+		list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 9, 10, 11 ) ) );
+		list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 10, 11 ) ) );
+		list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 11 ) ) );
+	}
+
+	[VSTesting.TestMethod]
+	public void RemoveAtTest4()
+	{
+		CircularList<int> list = new( 2 );
+		for( int i = 0; i < 8; i++ )
+			list.Add( i );
+		for( int i = 0; i < 7; i++ )
+			list.RemoveAt( 0 );
+		list.Add( 8 );
+		list.Add( 9 );
+		list.Add( 10 );
+		list.Add( 11 );
+		list.Add( 12 );
+		Assert( list.SequenceEqual( EnumerableOf( 7, 8, 9, 10, 11, 12 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 7, 9, 10, 11, 12 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 7, 10, 11, 12 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 7, 11, 12 ) ) );
+		list.RemoveAt( 0 );
+		Assert( list.SequenceEqual( EnumerableOf( 11, 12 ) ) );
+		list.RemoveAt( 1 );
+		Assert( list.SequenceEqual( EnumerableOf( 11 ) ) );
+	}
+
+	[VSTesting.Ignore] //too slow
+	[VSTesting.TestMethod]
+	public void RandomInsertTest()
+	{
+		int count = 12345;
+		MutableList<int> refList = new( 33 );
+		CircularList<int> list = new( 33 );
+		setupRandomTest( refList, list, count );
+
+		Sys.Random random = new( 3214 );
+		for( int i = 0; i < count; ++i )
+		{
+			int index = random.Next( refList.Count );
+			int value = random.Next();
+
+			list.Insert( index, value );
+			refList.Insert( index, value );
+
+			try
+			{
+				Assert( refList.SequenceEqual( list ) );
+			}
+			catch( Sys.Exception )
+			{
+				Sys.Console.WriteLine( $"Case: i={i},index={index},value={value}" );
+				printResult( refList, list );
+				throw;
+			}
+		}
+	}
+
+	[VSTesting.Ignore] //too slow
+	[VSTesting.TestMethod]
+	public void RandomRemoveTest()
+	{
+		int count = 10248;
+		MutableList<int> refList = new( 33 );
+		CircularList<int> list = new( 33 );
+		setupRandomTest( refList, list, count );
+
+		Sys.Random random = new( 3456345 );
+		for( int i = 0; i < count; ++i )
+		{
+			int index = random.Next( refList.Count );
+			int value = random.Next();
+
+			list.RemoveAt( index );
+			refList.RemoveAt( index );
+
+			try
+			{
+				Assert( refList.SequenceEqual( list ) );
+			}
+			catch( Sys.Exception )
+			{
+				Sys.Console.WriteLine( $"Case: i={i},index={index},value={value}" );
+				printResult( refList, list );
+				throw;
+			}
+		}
+	}
+
+	[VSTesting.Ignore] //too slow
+	[VSTesting.TestMethod]
+	public void RandomInsertRemoveTest()
+	{
+		int count = 20248;
+		MutableList<int> refList = new( 33 );
+		CircularList<int> list = new( 33 );
+		setupRandomTest( refList, list, count );
+		Sys.Random random = new( 123456 );
+		for( int i = 0; i < count; ++i )
+		{
+			ListAction action = (ListAction)random.Next( 3 );
+			int index = random.Next( refList.Count );
+			int value = random.Next();
+			if( action == ListAction.REMOVE_AT )
+			{
+				list.RemoveAt( index );
+				refList.RemoveAt( index );
+			}
+			else if( action == ListAction.REPLACE )
+			{
+				list[index] = value;
+				refList[index] = value;
+			}
+			else//insert
+			{
+				list.Insert( index, value );
+				refList.Insert( index, value );
+			}
+			try
+			{
+				Assert( refList.SequenceEqual( list ) );
+			}
+			catch( Sys.Exception )
+			{
+				Sys.Console.WriteLine( $"Case: i={i},action={action},index={index},value={value}" );
+				printResult( refList, list );
+				throw;
+			}
+		}
+	}
+
+	static void setupRandomTest( MutableList<int> refList, CircularList<int> list, int count )
+	{
+		Sys.Random random = new( 123456 );
+
+		for( int i = 0; i < count; ++i )
+		{
+			int value = random.Next();
+			list.Add( value );
+			refList.Add( value );
+		}
+
+		for( int i = 0; i < count / 2; ++i )
+		{
+			list.RemoveAt( 0 );
+			refList.RemoveAt( 0 );
+		}
+
+		for( int i = 0; i < count / 2; ++i )
+		{
+			int value = random.Next();
+			list.Add( value );
+			refList.Add( value );
+		}
+
+		try
+		{
+			Assert( refList.SequenceEqual( list ) );
+		}
+		catch( Sys.Exception )
+		{
+			printResult( refList, list );
+			throw;
+		}
+	}
+
+	static void printResult( MutableList<int> refList, CircularList<int> list )
+	{
+		Sys.Console.WriteLine( $"Expected Count:{refList.Count}, Actual Count:{list.Count} " );
+		int count = 0;
+		for( int i = 0; i < Sys.Math.Min( list.Count, refList.Count ); i++ )
+		{
+			if( list[i] != refList[i] )
+			{
+				Sys.Console.WriteLine( $"At index {i}, Expected Value:{refList[i]}, Actual Value:{list[i]}" );
+				count++;
+				if( count >= 10 )
+				{
+					Sys.Console.WriteLine( "...." );
+					break;
+				}
+			}
+		}
+	}
+
+	internal enum ListAction
+	{
+		REMOVE_AT,
+		REPLACE,
+		INSERT
+	}
+}
---
+++ MikeNakis.Kit/Collections/CircularList.cs
@@ -0,0 +1,269 @@
+namespace MikeNakis.Kit.Collections;
+
+using Sys = System;
+using System.Collections;
+using SysCompiler = System.Runtime.CompilerServices;
+
+/// <summary>
+/// A double-ended queue implements IList interface.
+/// </summary>
+/// <typeparam name="T">Type</typeparam>
+/// <remarks>
+/// O(1) Insert/RemoveAt for index 0 and n-1
+/// Originally from https://github.com/ewgdg/circular-list/blob/main/CircularList/CircularList.cs
+/// </remarks>
+public sealed class CircularList<T> : IList<T>
+{
+	T[] items;
+	int headIndex;
+	int tailIndex;
+	public int Count { get; private set; }
+	public bool IsReadOnly => false;
+
+	public CircularList() : this( 16 )
+	{
+	}
+
+	public CircularList( int initialCapacity )
+	{
+		Assert( initialCapacity < int.MaxValue );
+		items = new T[initialCapacity];
+		tailIndex = -1;
+	}
+
+	public T this[int index]
+	{
+		get
+		{
+			Assert( index >= 0 && index < Count );
+			int internalIndex = getInternalIndex( index );
+			return items[internalIndex];
+		}
+		set
+		{
+			Assert( index >= 0 && index < Count );
+			int internalIndex = getInternalIndex( index );
+			items[internalIndex] = value;
+		}
+	}
+
+	public void Add( T item )
+	{
+		if( Count >= items.Length )
+			expandCapacity();
+		tailIndex = increaseInternalIndex( tailIndex );
+		items[tailIndex] = item;
+		++Count;
+	}
+
+	public void Clear()
+	{
+		Count = 0;
+		headIndex = 0;
+		tailIndex = -1;
+	}
+
+	public bool Contains( T? item )
+	{
+		foreach( T existingItem in this )
+			if( EqualityComparer<T>.Default.Equals( existingItem, item ) )
+				return true;
+		return false;
+	}
+
+	public void CopyTo( T[] array, int arrayIndex )
+	{
+		if( Count == 0 )
+			return;
+		int copyCount = largestIndex - headIndex + 1;
+		Sys.Array.Copy( items, headIndex, array, arrayIndex, copyCount );
+
+		if( isWrapped( tailIndex ) )
+			Sys.Array.Copy( items, 0, array, arrayIndex + copyCount, tailIndex + 1 );
+	}
+
+	public IEnumerator<T> GetEnumerator()
+	{
+		for( int i = 0, internalIndex = getInternalIndex( i ); i < Count; i++, internalIndex = increaseInternalIndex( internalIndex ) )
+			yield return items[internalIndex];
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	public int IndexOf( T item )
+	{
+		int i = 0;
+		foreach( T existingItem in this )
+		{
+			if( EqualityComparer<T>.Default.Equals( existingItem, item ) )
+				return i;
+			i += 1;
+		}
+		return -1;
+	}
+
+	public void Insert( int index, T item )
+	{
+		Assert( index >= 0 && index < Count );
+
+		if( Count >= items.Length )
+			expandCapacity();
+
+		if( index == 0 || index < (Count - 1) / 2 )
+		{
+			int internalIndex = getInternalIndex( index );
+			int insertAtInternalIndex = decreaseInternalIndex( internalIndex );
+			int newHead = decreaseInternalIndex( headIndex );
+
+			if( index > 0 )
+			{
+				bool isInsertAtIndexWrapped = isWrapped( insertAtInternalIndex );
+				items[newHead] = items[headIndex];
+				int copyCount = (isInsertAtIndexWrapped ? items.Length - 1 : insertAtInternalIndex) - headIndex;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, headIndex + 1, items, headIndex, copyCount );
+
+				if( isInsertAtIndexWrapped )
+				{
+					items[^1] = items[0];
+					int copyCount2 = insertAtInternalIndex;
+					if( copyCount2 > 0 )
+						Sys.Array.Copy( items, 1, items, 0, copyCount2 );
+				}
+			}
+
+			headIndex = newHead;
+			items[insertAtInternalIndex] = item;
+			++Count;
+		}
+		else
+		{
+			int internalIndex = getInternalIndex( index );
+			if( isWrapped( internalIndex ) )
+			{
+				int copyCount = tailIndex - internalIndex + 1;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, internalIndex, items, internalIndex + 1, copyCount );
+			}
+			else
+			{
+				if( isWrapped( tailIndex ) )
+					Sys.Array.Copy( items, 0, items, 1, tailIndex + 1 );
+
+				int largestIndex = this.largestIndex;
+				items[increaseInternalIndex( largestIndex )] = items[largestIndex];
+				int copyCount = largestIndex - internalIndex;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, internalIndex, items, internalIndex + 1, copyCount );
+			}
+
+			tailIndex = increaseInternalIndex( tailIndex );
+			items[internalIndex] = item;
+			++Count;
+		}
+	}
+
+	public bool Remove( T item )
+	{
+		int index = IndexOf( item );
+		if( index < 0 )
+			return false;
+		RemoveAt( index );
+		return true;
+	}
+
+	public void RemoveAt( int index )
+	{
+		Assert( index >= 0 && index < Count );
+
+		if( index == 0 || index < (Count - 1) / 2 )
+		{
+			int internalIndex = getInternalIndex( index );
+			if( !isWrapped( internalIndex ) )
+			{
+				int copyCount = internalIndex - headIndex;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, headIndex, items, headIndex + 1, copyCount );
+			}
+			else
+			{
+				int copyCount = internalIndex;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, 0, items, 1, copyCount );
+				items[0] = items[^1];
+				int copyCount2 = items.Length - 1 - headIndex;
+				if( copyCount2 > 0 )
+					Sys.Array.Copy( items, headIndex, items, headIndex + 1, copyCount2 );
+			}
+
+			Count--;
+			headIndex = increaseInternalIndex( headIndex );
+		}
+		else
+		{
+			int internalIndex = getInternalIndex( index );
+			if( isWrapped( internalIndex ) )
+			{
+				//Array.Copy can handle overlap
+				int copyCount = tailIndex - internalIndex;
+				if( copyCount > 0 )
+					Sys.Array.Copy( items, internalIndex + 1, items, internalIndex, copyCount );
+			}
+			else
+			{
+				int copyCount = largestIndex - internalIndex;
+				Sys.Array.Copy( items, internalIndex + 1, items, internalIndex, copyCount );
+				if( isWrapped( tailIndex ) )
+				{
+					items[^1] = items[0];
+					int copyCount2 = tailIndex;
+					if( copyCount2 > 0 )
+						Sys.Array.Copy( items, 1, items, 0, tailIndex );
+				}
+			}
+
+			Count--;
+			tailIndex = decreaseInternalIndex( tailIndex );
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int largestIndex => isWrapped( tailIndex ) ? items.Length - 1 : tailIndex;
+
+	void expandCapacity()
+	{
+		int capacity = items.Length * 2;
+		if( (uint)capacity >= int.MaxValue )
+			capacity = int.MaxValue - 1;
+		if( capacity == items.Length )
+			throw new AssertionFailureException(); //maximum size limit reached.
+		T[] newItems = new T[capacity];
+		CopyTo( newItems, 0 );
+		items = newItems;
+		headIndex = 0;
+		tailIndex = Count - 1;
+	}
+
+	[SysCompiler.MethodImpl( SysCompiler.MethodImplOptions.AggressiveInlining )]
+	bool isWrapped( int internalIndex ) => internalIndex < headIndex;
+
+	[SysCompiler.MethodImpl( SysCompiler.MethodImplOptions.AggressiveInlining )]
+	int correctInternalIndex( int internalIndex )
+	{
+		if( internalIndex >= items.Length || internalIndex < 0 )
+			internalIndex %= items.Length;
+		if( internalIndex < 0 )
+			internalIndex += items.Length;
+		return internalIndex;
+	}
+
+	[SysCompiler.MethodImpl( SysCompiler.MethodImplOptions.AggressiveInlining )]
+	int getInternalIndex( int index ) => correctInternalIndex( index + headIndex );
+
+	[SysCompiler.MethodImpl( SysCompiler.MethodImplOptions.AggressiveInlining )]
+	int increaseInternalIndex( int internalIndex ) => correctInternalIndex( internalIndex + 1 );
+
+	[SysCompiler.MethodImpl( SysCompiler.MethodImplOptions.AggressiveInlining )]
+	int decreaseInternalIndex( int internalIndex ) => correctInternalIndex( internalIndex - 1 );
+}
